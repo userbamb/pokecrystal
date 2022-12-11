fcb7dbc1ac29aa60332e56249fddfa4db7f1447d
userbamb <110114819+userbamb@users.noreply.github.com>
2022-12-11 16:16:54 +0100

    dvs trainers available

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -6051,20 +6051,26 @@ LoadEnemyMon:
 	jp .Happiness
 
 .InitDVs:
-; Trainer DVs
 
-; All trainers have preset DVs, determined by class
-; See GetTrainerDVs for more on that
-	farcall GetTrainerDVs
-; These are the DVs we'll use if we're actually in a trainer battle
 	ld a, [wBattleMode]
 	dec a
-	jr nz, .UpdateDVs
+	jr z, .WildDVs
+
+; Trainer DVs
+	ld a, [wCurPartyMon]
+	ld hl, wOTPartyMon1DVs
+	call GetPartyLocation
+	ld b, [hl]
+	inc hl
+	ld c, [hl]
+	jr .UpdateDVs
+
+.WildDVs:
 
 ; Wild DVs
 ; Here's where the fun starts
 
-; Roaming monsters (Entei, Raikou) work differently
+; Roaming monsters (Entei, Raikou, Mew) work differently
 ; They have their own structs, which are shorter than normal
 	ld a, [wBattleType]
 	cp BATTLETYPE_ROAMING
