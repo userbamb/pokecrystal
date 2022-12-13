a4ff8785762530c8375a9acdf57e751bc0cd6f71
userbamb <110114819+userbamb@users.noreply.github.com>
2022-12-13 17:49:40 +0100

    Update CeladonMansionRoof.asm

--- maps/CeladonMansionRoof.asm
+++ maps/CeladonMansionRoof.asm
@@ -1,27 +1,121 @@
 	object_const_def
 	const CELADONMANSIONROOF_FISHER
+	const CELADONMANSIONROOF_ARTICUNO
+	const CELADONMANSIONROOF_ZAPDOS
+	const CELADONMANSIONROOF_MOLTRES
 
 CeladonMansionRoof_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	callback MAPCALLBACK_OBJECTS, CeladonMansionArticunoCallback
+    callback MAPCALLBACK_OBJECTS, CeladonMansionZapdosCallback
+    callback MAPCALLBACK_OBJECTS, CeladonMansionMoltresCallback
 
-CeladonMansionRoofFisherScript:
-	jumptextfaceplayer CeladonMansionRoofFisherText
+CeladonMansionarticunoCallback:
+    checkevent EVENT_GOT_ARTICUNO
+	iftrue .Nospawn
+	readvar VAR_WEEKDAY
+	ifequal MONDAY, .Appear
+	sjump .NoAppear
+
+.Appear:
+	appear CELADONMANSIONROOF_ARTICUNO
+	endcallback
+
+.NoAppear:
+	disappear CELADONMANSIONROOF_ARTICUNO
+	endcallback
+
+Articuno:
+	faceplayer
+	opentext
+	writetext ArticunoText
+	cry ARTICUNO
+	pause 15
+	closetext
+	setevent EVENT_GOT_ARTICUNO
+	loadvar VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadwildmon ARTICUNO, 60
+	startbattle
+	disappear CELADONMANSIONROOF_ARTICUNO
+	reloadmapafterbattle
+	end
+
+ArticunoText:
+	text "Pyaaas!"
+	done
+
+CeladonMansionZapdosCallback:
+    checkevent EVENT_GOT_ZAPDOS
+	iftrue .Nospawn
+	readvar VAR_WEEKDAY
+	ifequal FRIDAY, .Appear
+	sjump .NoAppear
+
+.Appear:
+	appear CELADONMANSIONROOF_ZAPDOS
+	endcallback
+
+.NoAppear:
+	disappear CELADONMANSIONROOF_ZAPDOS
+	endcallback
+
+Zapdos:
+	faceplayer
+	opentext
+	writetext ZapdosText
+	cry ZAPDOS
+	pause 15
+	closetext
+	setevent EVENT_GOT_ZAPDOS
+	loadvar VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadwildmon ZAPDOS, 60
+	startbattle
+	disappear CELADONMANSIONROOF_ZAPDOS
+	reloadmapafterbattle
+	end
+
+ZapdosText:
+	text "Tyaaas!"
+	done
+
+CeladonMansionMoltresCallback:
+    checkevent EVENT_GOT_MOLTRES
+	iftrue .Nospawn
+	readvar VAR_WEEKDAY
+	ifequal SUNDAY, .Appear
+	sjump .NoAppear
+
+.Appear:
+	appear CELADONMANSIONROOF_MOLTRES
+	endcallback
+
+.NoAppear:
+	disappear CELADONMANSIONROOF_MOLTRES
+	endcallback
+
+Moltres:
+	faceplayer
+	opentext
+	writetext MoltresText
+	cry MOLTRES
+	pause 15
+	closetext
+	setevent EVENT_GOT_MOLTRES
+	loadvar VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadwildmon MOLTRES, 60
+	startbattle
+	disappear CELADONMANSIONROOF_MOLTRES
+	reloadmapafterbattle
+	end
+
+MoltresText:
+	text "Nyaaas!"
+	done
 
 CeladonMansionRoofGraffiti:
 	jumptext CeladonMansionRoofGraffitiText
-
-CeladonMansionRoofFisherText:
-	text "High places--I do"
-	line "love them so!"
-
-	para "I'd say the only"
-	line "thing that loves"
-
-	para "heights as much as"
-	line "me is smoke!"
-	done
 
 CeladonMansionRoofGraffitiText:
 	text "There's graffiti"
@@ -45,4 +139,7 @@ CeladonMansionRoof_MapEvents:
 	bg_event  6,  1, BGEVENT_LEFT, CeladonMansionRoofGraffiti
 
 	def_object_events
-	object_event  7,  5, SPRITE_FISHER, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, CeladonMansionRoofFisherScript, -1
+	object_event 5, 10, SPRITE_ARTICUNO, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Articuno, 0
+    object_event 5, 10, SPRITE_ZAPDOS, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, Zapdos, 0
+	object_event 5, 10, SPRITE_MOLTRES, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Moltres, 0
+    
