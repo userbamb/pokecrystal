475cb3a1da11fc5c9e7880b2c768b65cc0432724
userbamb <110114819+userbamb@users.noreply.github.com>
2023-04-19 18:32:25 +0200

    Update names.asm

--- data/pokemon/names.asm
+++ data/pokemon/names.asm
@@ -252,7 +252,7 @@ PokemonNames::
 	db "HO-OH@@@@@"
 	db "CELEBI@@@@"
 	db "METTO@@@@@"
-	db "DRAGONHIGH"
+	db "DRAGONITE@"
 	assert_table_length NUM_POKEMON
 	db "EGG@@@@@@@"
 	assert_table_length EGG
