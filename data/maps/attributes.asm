4b56a26319ddd74baf788d5bf61a9483d5280f74
userbamb <110114819+userbamb@users.noreply.github.com>
2023-12-23 17:01:54 +0100

    fix

--- data/maps/attributes.asm
+++ data/maps/attributes.asm
@@ -715,6 +715,6 @@ ENDM
 	map_attributes RuinUnderwater, RUIN_UNDERWATER, $09, 0
 	map_attributes UnderwaterLabi, UNDERWATER_LABI, $09, 0
 	map_attributes CinnabarUnderwater, CINNABAR_UNDERWATER, $09, 0
-	map_attributes Cinnabar1F, CINNABAR_1F, $14, 0
+	map_attributes Cinnabar1F, CINNABAR_1F, $17, 0
 	map_attributes CianwoodUnderwater, CIANWOOD_UNDERWATER, $09, 0
 	map_attributes Route41Underwater, ROUTE_41_UNDERWATER, $09, 0
